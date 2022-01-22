cb20744ce9ef39cfaa44508b8d315cb1e5ee78a1
revan129 <80680765+revan129@users.noreply.github.com>
2022-01-22 09:43:06 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip install requests
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip install mechanize
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip install requests bs4
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip2 install requests
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip2 install mechanize
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+apt install wget -y
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+apt install ruby -y
+clear
+echo
+echo "          ╱◢╱╱╱◣╲╱◢╲╲╲◣╲   ╱◢╱╱╱◣╲╱◢╲╲╲◣╲"
+echo "          ◢▇◣╱╱▇▇▇▇╲╲◢▇◣   ◢▇◣╱╱▇▇▇▇╲╲◢▇◣"
+echo "          ▇▇▇▇▇▇▇▇▇▇▇▇▇▇   ▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+echo "              R 3 V 4 N          4 K B 4 R"
+echo "          ╲◥╲╲╲╲◥◤╱╱╱╱◤╱   ╲◥╲╲╲╲◥◤╱╱╱╱◤╱"
+echo
+gem install lolcat
+clear
+echo
+echo "         ╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" R| lolcat
+echo "         ╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" E| lolcat
+echo "         ┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" V| lolcat
+echo "         ┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" A| lolcat
+echo "         ╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" N| lolcat
+echo "         ╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" 7| lolcat
+echo
+echo
+clear
+echo
+echo "     FUCK YEAHHH!"
+echo "     [√] memasang selesai..."
+echo
+echo "   ketik python v4n.py untuk melanjutkan"
+echo#!/bin/bash
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip install requests
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip install mechanize
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip install requests bs4
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip2 install requests
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+pip2 install mechanize
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+apt install wget -y
+clear
+echo
+echo "        ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗"
+echo "        ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ "
+echo "        ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ "
+echo
+apt install ruby -y
+clear
+echo
+echo "          ╱◢╱╱╱◣╲╱◢╲╲╲◣╲   ╱◢╱╱╱◣╲╱◢╲╲╲◣╲"
+echo "          ◢▇◣╱╱▇▇▇▇╲╲◢▇◣   ◢▇◣╱╱▇▇▇▇╲╲◢▇◣"
+echo "          ▇▇▇▇▇▇▇▇▇▇▇▇▇▇   ▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+echo "              R 3 V 4 N          4 K B 4 R"
+echo "          ╲◥╲╲╲╲◥◤╱╱╱╱◤╱   ╲◥╲╲╲╲◥◤╱╱╱╱◤╱"
+echo
+gem install lolcat
+clear
+echo
+echo "         ╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" R| lolcat
+echo "         ╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" E| lolcat
+echo "         ┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" V| lolcat
+echo "         ┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" A| lolcat
+echo "         ╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" N| lolcat
+echo "         ╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" 7| lolcat
+echo
+echo
+clear
+echo
+echo "     FUCK YEAHHH!"
+echo "     [√] memasang selesai..."
+echo
+echo "   ketik python v4n.py untuk melanjutkan"
+echo
